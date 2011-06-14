7c1b9440ebd99eaa0661157929c1a44b1c4e0a39
Tony Stubblebine <tony@tonystubblebine.com>
2011-06-14 14:05:33 -0700

    Make default comment submit text be "Comment"

--- app/controllers/big_comment/comments_controller.rb
+++ app/controllers/big_comment/comments_controller.rb
@@ -49,7 +49,8 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         @comment.commentable.comments.map{|c| c.user}.uniq.each do |user|
-          CommentNotifier.deliver_new_comment(@comment, user) if (user.email and BigComment.from_email_address and user != current_user)
+          next unless (user.email and BigComment.from_email_address and user != current_user)
+          CommentNotifier.deliver_new_comment(@comment, user)
         end
         format.html { redirect_to(@comment, :notice => 'Comment was successfully created.') }
         format.xml  { render :xml => @comment, :status => :created, :location => @comment }
